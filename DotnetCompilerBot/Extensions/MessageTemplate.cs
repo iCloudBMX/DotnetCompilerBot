504c60a72f8c1641830ab3021c8bbf55bab4e7bd
Khondamir Abdukhoshimov <iCloudBMX0525@gmail.com>
2023-03-13 17:28:11 +0500

    Added message template

---
+++ DotnetCompilerBot/Extensions/MessageTemplate.cs
@@ -0,0 +1,21 @@
+﻿using DotnetCompilerBot.Models;
+using System.Text.RegularExpressions;
+
+namespace DotnetCompilerBot.Extensions
+{
+    public static class MessageTemplate
+    {
+        public static string GetDecoratedMessage(
+            string message,
+            DecoraterType decoraterType)
+        {
+            string decoratedMessage = decoraterType switch
+            {
+                DecoraterType.Bold => $"<b>{message}</b>",
+                DecoraterType.Monospace => $"<pre>{message}</pre>"
+            };
+
+            return decoratedMessage;
+        }
+    }
+}
